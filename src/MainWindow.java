dcdb21dc48c2ecf6105d8e25456624855d7d63f9
kiiraklis94 <kiiraklis94@gmail.com>
2015-07-19 02:12:58 +0300

    Added Steam overlay option, and streamlined the app.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -24,6 +24,7 @@ import javax.swing.UIManager;
 import javax.swing.JTextPane;
 import java.awt.SystemColor;
 import javax.swing.JTextField;
+import javax.swing.JSeparator;
 
 
 //Experimental
@@ -31,8 +32,11 @@ public class MainWindow {
 	
 	static String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 	static String sizes[] = new String[51];
+	static String overlaySizes[] = new String [51];
+	
 	static String fontSelected;
 	static String sizeSelected;
+	static String overlaySizeSelected;
 	final JFileChooser fileSelect = new JFileChooser();
 	String steamFolder;
 	private JFrame frmSteamFontChanger;
@@ -45,6 +49,7 @@ public class MainWindow {
 		
 		for(int i=1;i<=50;i++){
 			sizes[i] = Integer.toString(i);
+			overlaySizes[i] = Integer.toString(i);
 		}
 		
 		try {
@@ -86,18 +91,31 @@ public class MainWindow {
 		frmSteamFontChanger.setTitle("Steam Font Changer");
 		frmSteamFontChanger.setResizable(false);
 		frmSteamFontChanger.setBackground(SystemColor.menu);
-		frmSteamFontChanger.setBounds(100, 100, 621, 437);
+		frmSteamFontChanger.setBounds(100, 100, 740, 517);
 		frmSteamFontChanger.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmSteamFontChanger.getContentPane().setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setFont(new Font("Verdana", Font.PLAIN, 14));
-		tabbedPane.setBounds(0, 0, 615, 408);
+		tabbedPane.setBounds(0, 0, 734, 651);
 		frmSteamFontChanger.getContentPane().add(tabbedPane);
+
+		JPanel tab1 = new JPanel();
+		tabbedPane.addTab("Change font and sizes", null, tab1, null);
+		tab1.setBackground(UIManager.getColor("Button.background"));
+		tab1.setLayout(null);
 		
-		JPanel tab0 = new JPanel();
-		tabbedPane.addTab("Steam Location", null, tab0, null);
-		tab0.setLayout(null);
+		JLabel lblSpecifySteamFolder = new JLabel("Specify Steam folder location");
+		lblSpecifySteamFolder.setFont(new Font("Verdana", Font.PLAIN, 13));
+		lblSpecifySteamFolder.setBounds(12, 11, 396, 33);
+		tab1.add(lblSpecifySteamFolder);
+		
+		textField = new JTextField(steamFolder);
+		textField.setEditable(false);
+		textField.setFont(new Font("Verdana", Font.PLAIN, 16));
+		textField.setBounds(12, 39, 489, 33);
+		tab1.add(textField);
+		textField.setColumns(10);
 		
 		JButton browseButton = new JButton("Browse");
 		browseButton.addActionListener(new ActionListener() {
@@ -108,43 +126,34 @@ public class MainWindow {
 				if(fileSelect.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
 					steamFolder = fileSelect.getSelectedFile().toString();
 					textField.setText(steamFolder);
-//					System.out.println(steamFolder);
-					tabbedPane.setSelectedIndex(1);
 				    } 
 				
 			}
 		});
-		browseButton.setBounds(423, 168, 175, 33);
-		tab0.add(browseButton);
+		browseButton.setBounds(510, 39, 207, 33);
+		tab1.add(browseButton);
 		
-		textField = new JTextField(steamFolder);
-		textField.setEditable(false);
-		textField.setFont(new Font("Verdana", Font.PLAIN, 16));
-		textField.setBounds(12, 168, 399, 33);
-		tab0.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblSpecifySteamFolder = new JLabel("Specify Steam folder location");
-		lblSpecifySteamFolder.setFont(new Font("Verdana", Font.PLAIN, 13));
-		lblSpecifySteamFolder.setBounds(12, 137, 399, 33);
-		tab0.add(lblSpecifySteamFolder);
-		
-		JPanel tab1 = new JPanel();
-		tabbedPane.addTab("Basefont and Size", null, tab1, null);
-		tab1.setBackground(UIManager.getColor("Button.background"));
-		tab1.setLayout(null);
+		JSeparator separator = new JSeparator();
+		separator.setBounds(12, 83, 705, 2);
+		tab1.add(separator);
 		
 		JLabel lblFontFamily = new JLabel("Basefont:");
 		lblFontFamily.setToolTipText("Default: Arial");
 		lblFontFamily.setFont(new Font("Verdana", Font.PLAIN, 16));
-		lblFontFamily.setBounds(10, 11, 465, 30);
+		lblFontFamily.setBounds(12, 96, 396, 21);
 		tab1.add(lblFontFamily);
 		
 		JLabel lblFontSize = new JLabel("Font Size:");
-		lblFontSize.setToolTipText("Default: 14 (may be more for some buttons etc)");
+		lblFontSize.setToolTipText("Default: 14 (usually)");
 		lblFontSize.setFont(new Font("Verdana", Font.PLAIN, 16));
-		lblFontSize.setBounds(487, 11, 113, 30);
+		lblFontSize.setBounds(420, 96, 105, 21);
 		tab1.add(lblFontSize);
+		
+		JLabel lblOverlayFontSize = new JLabel("Overlay Font Size:");
+		lblOverlayFontSize.setToolTipText("Default: 28");
+		lblOverlayFontSize.setFont(new Font("Verdana", Font.PLAIN, 16));
+		lblOverlayFontSize.setBounds(537, 96, 180, 21);
+		tab1.add(lblOverlayFontSize);
 		
 		JList fontList = new JList(fonts);
 		fontList.setSelectedIndex(0);
@@ -158,7 +167,7 @@ public class MainWindow {
 		lblFontFamily.setLabelFor(scrollFont);
 		scrollFont.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollFont.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollFont.setBounds(10, 41, 465, 281);
+		scrollFont.setBounds(12, 121, 396, 281);
 		tab1.add(scrollFont);
 		
 		JList sizeList = new JList(sizes);
@@ -172,16 +181,30 @@ public class MainWindow {
 		lblFontSize.setLabelFor(scrollSize);
 		scrollSize.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollSize.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollSize.setBounds(487, 41, 113, 281);
+		scrollSize.setBounds(420, 121, 105, 281);
 		tab1.add(scrollSize);
 		
+		JList overlaySizeList = new JList(overlaySizes);
+		overlaySizeList.setSelectedIndex(1);
+		overlaySizeList.setFont(new Font("Verdana", Font.PLAIN, 16));
+		overlaySizeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		overlaySizeList.setBounds(535, 41, 182, 281);
+		tab1.add(overlaySizeList);
+		
+		JScrollPane scrollOverlaySize = new JScrollPane(overlaySizeList);
+		scrollOverlaySize.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollOverlaySize.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollOverlaySize.setBounds(537, 121, 182, 281);
+		tab1.add(scrollOverlaySize);
+		
 		JButton fontButton = new JButton("Confirm Selection");
-		fontButton.setBounds(10, 333, 590, 33);
+		fontButton.setBounds(12, 413, 707, 33);
 		tab1.add(fontButton);
 		fontButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				fontSelected = fontList.getSelectedValue().toString();
 				sizeSelected = sizeList.getSelectedValue().toString();
+				overlaySizeSelected = overlaySizeList.getSelectedValue().toString();
 				doit();
 			}
 		});
@@ -197,11 +220,11 @@ public class MainWindow {
 		aboutText.setEditable(false);
 		aboutText.setFont(new Font("Verdana", Font.PLAIN, 16));
 		aboutText.setText("-----------------------------------\r\nSteam Font Changer\r\n-----------------------------------\r\nApp created by kiiraklis94\r\n\r\nThis application allows you to change how your steam client looks by changing the default font to whatever you like.\r\n\r\nIt may help you make steam more readable or whatever, I don't know.\r\n\r\nMore features (may be) coming soon...\r\n\r\n-----------------------------------\r\nHow to use SFC\r\n-----------------------------------\r\n1. Specify where Steam is installed by selecting the Steam folder in the first tab.\r\n\r\n2. Choose the font and font size you want from the lists. \r\n(The listed fonts are those that are available in your system, so if you have installed a font, it will appear in the list (probably... at least it's supposed to).)\r\n\r\n2. Click on the button \"Confirm Selection\".\r\n\r\n3. SFC will create a new theme/skin for you (called SFC_Font), and will place it in the correct folder automatically. \r\n\r\n4. After that, restart Steam, and the new theme will be available for you to apply.");
-		aboutText.setBounds(10, 11, 590, 355);
+		aboutText.setBounds(1, 1, 727, 415);
 		tab2.add(aboutText);
 		
 		JScrollPane scrollPane_2 = new JScrollPane(aboutText);
-		scrollPane_2.setBounds(0, 0, 610, 374);
+		scrollPane_2.setBounds(0, 0, 729, 458);
 		tab2.add(scrollPane_2);
 	}
 
@@ -211,17 +234,30 @@ public class MainWindow {
 		BufferedReader br = null;
 	    BufferedWriter bw = null;
 	    
+		BufferedReader br2 = null;
+	    BufferedWriter bw2 = null;
+	    
+	    //Client
     	File steamDefaultFile = new File(steamFolder + "/resource/styles/steam.styles");
 	    File steamFile = new File(steamFolder + "/skins/SFC_Font/resource/styles/steam.styles");
+	    //Overlay
+    	File overlayDefaultFile = new File(steamFolder + "/resource/styles/gameoverlay.styles");
+    	File overlayFile = new File(steamFolder + "/skins/SFC_Font/resource/styles/gameoverlay.styles");
     	
 	    try{
 	    	
 	    	steamFile.getParentFile().mkdirs();
+	    	overlayFile.getParentFile().mkdirs();
 	    	
 	        br = new BufferedReader(new FileReader(steamDefaultFile));
 	        bw = new BufferedWriter(new FileWriter(steamFile));
 	        
+	        br2 = new BufferedReader(new FileReader(overlayDefaultFile));
+	        bw2 = new BufferedWriter(new FileWriter(overlayFile));
+	        
 	         
+	        // Do it for steam.styles
+	        
 	        String line = br.readLine();
 	        
 	        for( int i = 1; i <= 35 && line != null; i++)
@@ -230,8 +266,6 @@ public class MainWindow {
 	            bw.write("\n");
 	            line = br.readLine();
 	        }
-	        
-	        
 	        for(int i = 36;i<=36 && line != null; i++){
 	        	bw.write("    basefont=\"");
 	        	bw.write(fontSelected);
@@ -249,7 +283,6 @@ public class MainWindow {
 	        	line = br.readLine();
 	        	
 	        }
-	        
 	        for (int i = 37; i<=3482 && line != null; i++ ){
 		         
 		        if(line.contains("font-size=")){
@@ -265,14 +298,41 @@ public class MainWindow {
 		        }
 	        }
 	        
-//	        System.out.println("The rest of the lines are Successfully copied!");
+	        //Do it for gameoverlay.styles
+	        
+	        line = br2.readLine();
+	        
+	        for (int i=1; i<=24 && line != null; i++){
+	        	
+	            bw2.write(line);
+	            bw2.write("\n");
+	            line = br2.readLine();
+	        }
+	        for (int i=25; i<=25 && line != null; i++){
+	        	
+		        bw2.write("			font-size=");
+	        	bw2.write(overlaySizeSelected);
+	        	bw2.write("\n");
+	        	line = br2.readLine();
+	        }
+	        for(int i=26; i<=31 && line != null; i++){
+	        	
+	            bw2.write(line);
+	            bw2.write("\n");
+	            line = br2.readLine();
+	        }
 	         
+	        
 	        br.close();
 	        bw.close();
-	        JOptionPane.showMessageDialog(frmSteamFontChanger, "Done");
+	        
+	        br2.close();
+	        bw2.close();
+	        
+	        JOptionPane.showMessageDialog(frmSteamFontChanger, "You can now change your Steam skin to \"SFC_Font\".", "Operation Completed Successfuly", JOptionPane.INFORMATION_MESSAGE);
 	    }
 	    catch(Exception e){
-	        System.out.println("Exception caught : " + e);
+	    	JOptionPane.showMessageDialog(frmSteamFontChanger, "You didn't specify the correct Steam location, or didn't specify a location at all.", "Error - File not found", JOptionPane.ERROR_MESSAGE);
 	    }
 
 		
